fb7dacada9456d8a0a41fdecae20b13fefb53656
Robert J. Macomber <robert.macomber@socrata.com>
2016-08-26 15:04:19 -0700

    Smarter behavior on bad ZK config values
    Instead of reverting to the default, remain at the most recent valid
    value if there was one.

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/util/ConfigWatch.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/util/ConfigWatch.scala
@@ -10,13 +10,19 @@ import org.apache.curator.framework.recipes.cache.{NodeCacheListener, NodeCache}
 import org.slf4j.LoggerFactory
 
 class ConfigWatch[T : JsonDecode](curator: CuratorFramework, path: String, default: T) extends (() => T) with Closeable { self =>
-  private val log = LoggerFactory.getLogger(classOf[ConfigWatch[_]])
+  import ConfigWatch._
 
   @volatile private var currentValue = Option.empty[T]
+  @volatile private var previousValue = Option.empty[T]
 
   private val cache = new NodeCache(curator, path)
   private val listener = new NodeCacheListener {
-    override def nodeChanged(): Unit = self.synchronized { currentValue = None }
+    override def nodeChanged(): Unit = self.synchronized {
+      if(currentValue != None) {
+        previousValue = currentValue
+        currentValue = None
+      }
+    }
   }
   cache.getListenable.addListener(listener)
 
@@ -39,6 +45,11 @@ class ConfigWatch[T : JsonDecode](curator: CuratorFramework, path: String, defau
             case Some(t) =>
               t
             case None =>
+              // Mildly complex logic here:
+              //    If the node is gone, use the default value
+              //    If the node exists but is not parsable for whatever reason, keep the previous value (or
+              //      the default if there was no valid previous value)
+              //    Otherwise use the new value and forget the previous value.
               val newData =
                 for {
                   data <- Option(cache.getCurrentData)
@@ -49,20 +60,26 @@ class ConfigWatch[T : JsonDecode](curator: CuratorFramework, path: String, defau
                       case Right(t) =>
                         Some(t)
                       case Left(error) =>
-                        log.warn("Undecodable data in config watch {}: {}", path: Any, error.english: Any)
-                        None
+                        log.warn("Undecodable data in config watch {}: {}", path: Any, error.english)
+                        previousValue
                     }
                   } catch {
                     case e: Exception =>
-                      log.warn("Unparsable data in config watch {}: {}", path: Any, e: Any)
-                      None
+                      log.warn("Unparsable data in config watch {}: {}", path: Any, e)
+                      previousValue
                   }
                 }
               val result = newData.flatten.getOrElse(default)
+              log.debug("Config watch {} now has value {}", path: Any, result)
               currentValue = Some(result)
+              previousValue = None
               result
           }
         }
     }
   }
+}
+
+object ConfigWatch {
+  private val log = LoggerFactory.getLogger(classOf[ConfigWatch[_]])
 }
