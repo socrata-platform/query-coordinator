403439fc755a8f5a521a54183bba736164bfe4d3
Chi Tang <chi.tang@socrata.com>
2017-11-20 11:21:31 -0800

    EN-20418 Remove foreign key constraint in query coordinator cache data tables.

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/caching/cache/postgresql/PostgresqlCacheSchema.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/caching/cache/postgresql/PostgresqlCacheSchema.scala
@@ -72,7 +72,7 @@ object PostgresqlCacheSchema {
           for (ds <- dataset) {
             val dataTableName = dataTable(ds)
             if (!exists(dataTableName)) {
-              stmt.execute(s"create table ${dataTableName} (cache_id bigint not null references $table(id), chunknum int not null, data bytea not null, primary key(cache_id, chunknum))")
+              stmt.execute(s"create table ${dataTableName} (cache_id bigint not null, chunknum int not null, data bytea not null, primary key(cache_id, chunknum))")
             }
           }
         }
