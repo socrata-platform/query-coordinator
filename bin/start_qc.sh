85e6ce05be48f9d2620413499780c2e924a5873a
Alexa Rust <alexa.rust@socrata.com>
2018-01-31 11:05:11 -0800

    EN-21594: Follow new NBE local config convention
    Follow new NBE local development config file convention.

--- bin/start_qc.sh
+++ bin/start_qc.sh
@@ -5,7 +5,7 @@ set -e
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BINDIR=$(dirname "$REALPATH")
 
-CONFIG="${SODA_CONFIG:-"$BINDIR/../config/query-coordinator.conf"}"
+CONFIG="${SODA_CONFIG:-"$BINDIR/../configs/application.conf"}"
 JARFILE=$("$BINDIR"/build.sh "$@")
 
 java -Djava.net.preferIPv4Stack=true -Dconfig.file="$CONFIG" -jar "$JARFILE"
