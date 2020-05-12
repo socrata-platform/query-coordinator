c2c3dd18b5e4ed7f1d4600de6dc95099a380cd77
Chi Tang <chi.tang@socrata.com>
2020-05-12 14:12:26 -0700

    EN-39970 Allow table aliases to be the same among individual selects … (#180)

---
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/UniqueTableAliasesTest.scala
@@ -0,0 +1,31 @@
+package com.socrata.querycoordinator
+
+import com.socrata.soql.parsing.Parser
+
+
+class UniqueTableAliasesTest extends TestBase {
+
+  /***
+    * The 2nd @b is renamed to @b__2 to avoid collision with the first @b
+    */
+  test("table aliases renamed so that analyze do not collide") {
+    val q = """
+            SELECT aa |>
+            SELECT @b.ee, max(aa) as max_aa, max(@b.dd) as max_dd JOIN @bbbb-bbbb as b ON aa=@b.aa group by @b.ee |>
+            SELECT @b.aa, max_aa, @b.ff JOIN @cccc-cccc as b ON aa=@b.aa ORDER BY @b.ff"""
+    val parsed0 = new Parser().selectStatement(q)
+
+
+    val expected = Seq(
+      "SELECT `aa`",
+      "SELECT @b.`ee`, max(`aa`) AS max_aa, max(@b.`dd`) AS max_dd JOIN @bbbb-bbbb AS b ON `aa` = @b.`aa` GROUP BY @b.`ee`",
+      "SELECT @b__2.`aa`, `max_aa`, @b__2.`ff` JOIN @cccc-cccc AS b__2 ON `aa` = @b__2.`aa` ORDER BY @b__2.`ff` ASC NULL LAST")
+
+    val parsed = UniqueTableAliases(parsed0)
+
+    expected.zip(parsed.map(_.toString).seq).foreach {
+      case (expectedSql, actualSql) =>
+        actualSql shouldBe(expectedSql)
+    }
+  }
+}
