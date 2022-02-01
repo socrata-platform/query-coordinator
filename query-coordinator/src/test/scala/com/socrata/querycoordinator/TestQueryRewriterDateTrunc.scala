a03917fd9efe1b42ff417fcd413c4d37601ea9d7
Chi Tang <chi.tang@socrata.com>
2022-02-01 10:29:34 -0800

    EN-50442: Rollups can work with filter (#237)

--- query-coordinator/src/test/scala/com/socrata/querycoordinator/TestQueryRewriterDateTrunc.scala
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/TestQueryRewriterDateTrunc.scala
@@ -3,50 +3,60 @@ package com.socrata.querycoordinator
 class TestQueryRewriterDateTrunc extends TestQueryRewriterDateTruncBase {
   val rewrittenQueryRymd = "SELECT c2 as ward, coalesce(sum(c3), 0) as count WHERE c1 BETWEEN date_trunc_ymd('2011-02-01') " +
     "AND date_trunc_ymd('2012-05-02') GROUP BY c2"
-  val rewrittenQueryAnalysisRymd = analyzeRewrittenQuery("r_ymd", rewrittenQueryRymd)
+  def rewrittenQueryAnalysisRymd = analyzeRewrittenQuery("r_ymd", rewrittenQueryRymd)
 
   val rewrittenQueryRym = "SELECT c2 as ward, coalesce(sum(c3), 0) as count WHERE c1 BETWEEN date_trunc_ym('2011-02-01') " +
     "AND date_trunc_ym('2012-05-02') GROUP BY c2"
-  val rewrittenQueryAnalysisRym = analyzeRewrittenQuery("r_ymd", rewrittenQueryRym)
+  def rewrittenQueryAnalysisRym = analyzeRewrittenQuery("r_ymd", rewrittenQueryRym)
 
   val rewrittenQueryNotBetweenRym = "SELECT c2 as ward, coalesce(sum(c3), 0) as count WHERE c1 NOT BETWEEN " +
     "date_trunc_ym('2011-02-01') AND date_trunc_ym('2012-05-02') GROUP BY c2"
-  val rewrittenQueryAnalysisNotBetweenRym = analyzeRewrittenQuery("r_ymd", rewrittenQueryNotBetweenRym)
+  def rewrittenQueryAnalysisNotBetweenRym = analyzeRewrittenQuery("r_ymd", rewrittenQueryNotBetweenRym)
 
   val rewrittenQueryRy = "SELECT c2 as ward, coalesce(sum(c3), 0) as count WHERE c1 BETWEEN date_trunc_y('2011-02-01') " +
     "AND date_trunc_y('2012-05-02') GROUP BY c2"
-  val rewrittenQueryAnalysisRy = analyzeRewrittenQuery("r_y", rewrittenQueryRy)
+  def rewrittenQueryAnalysisRy = analyzeRewrittenQuery("r_y", rewrittenQueryRy)
+
+
+  val qDateTruncOnLiterals = "SELECT ward, count(*) as count WHERE crime_date BETWEEN '2004-01-01' AND '2014-01-01' GROUP BY ward"
 
   test("Shouldn't rewrite date_trunc on literals") {
-    val q = "SELECT ward, count(*) as count WHERE crime_date BETWEEN '2004-01-01' AND '2014-01-01' GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+    val queryAnalysis = analyzeQuery(qDateTruncOnLiterals)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
     rewrites should have size 0
   }
+
+  val qDifferingAggregations = """
+    SELECT ward, count(*) as count
+     WHERE crime_date BETWEEN date_trunc_y('2004-01-01') AND date_trunc_ym('2014-01-01') GROUP BY ward"""
 
   test("Shouldn't rewrite date_trunc on differing aggregations") {
-    val q = "SELECT ward, count(*) as count WHERE crime_date BETWEEN date_trunc_y('2004-01-01') " +
-      "AND date_trunc_ym('2014-01-01') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+    val queryAnalysis = analyzeQuery(qDifferingAggregations)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
     rewrites should have size 0
   }
+
+  val qDateTruncCannotBeMapped = """
+    SELECT ward, count(*) as count
+     WHERE crime_date BETWEEN date_trunc_y(some_date) AND date_trunc_y('2014-01-01') GROUP BY ward"""
 
   test("Shouldn't rewrite date_trunc on expression that can't be mapped") {
-    val q = "SELECT ward, count(*) as count WHERE crime_date BETWEEN date_trunc_y(some_date) " +
-      "AND date_trunc_y('2014-01-01') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+
+    val queryAnalysis = analyzeQuery(qDateTruncCannotBeMapped)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
     rewrites should have size 0
   }
 
+  val qBetweenDateTruncYmd = """
+    SELECT ward, count(*) AS count
+     WHERE crime_date BETWEEN date_trunc_ymd('2011-02-01') AND date_trunc_ymd('2012-05-02')
+     GROUP BY ward"""
+
   test("BETWEEN date_trunc_ymd") {
-    val q = "SELECT ward, count(*) AS count WHERE crime_date BETWEEN date_trunc_ymd('2011-02-01') " +
-      "AND date_trunc_ymd('2012-05-02') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+    val queryAnalysis = analyzeQuery(qBetweenDateTruncYmd)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
 
@@ -56,11 +66,13 @@ class TestQueryRewriterDateTrunc extends TestQueryRewriterDateTruncBase {
     rewrites should have size 1
   }
 
+  val qBetweenDateTruncYm = """
+     SELECT ward, count(*) AS count
+      WHERE crime_date BETWEEN date_trunc_ym('2011-02-01') AND date_trunc_ym('2012-05-02')
+      GROUP BY ward"""
 
   test("BETWEEN date_trunc_ym") {
-    val q = "SELECT ward, count(*) AS count WHERE crime_date BETWEEN date_trunc_ym('2011-02-01') " +
-      "AND date_trunc_ym('2012-05-02') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+    val queryAnalysis = analyzeQuery(qBetweenDateTruncYm)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
 
@@ -72,11 +84,13 @@ class TestQueryRewriterDateTrunc extends TestQueryRewriterDateTruncBase {
     rewrites should have size 2
   }
 
+  val qBetweenDateTruncY = """
+    SELECT ward, count(*) AS count
+     WHERE crime_date BETWEEN date_trunc_y('2011-02-01') AND date_trunc_y('2012-05-02') GROUP BY ward"""
 
   test("BETWEEN date_trunc_y") {
-    val q = "SELECT ward, count(*) AS count WHERE crime_date BETWEEN date_trunc_y('2011-02-01') " +
-      "AND date_trunc_y('2012-05-02') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+
+    val queryAnalysis = analyzeQuery(qBetweenDateTruncY)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
 
@@ -90,11 +104,14 @@ class TestQueryRewriterDateTrunc extends TestQueryRewriterDateTruncBase {
     rewrites should have size 3
   }
 
+  val qNotBetweenDateTruncYm = """
+    SELECT ward, count(*) AS count
+     WHERE crime_date NOT BETWEEN date_trunc_ym('2011-02-01') AND date_trunc_ym('2012-05-02')
+     GROUP BY ward"""
 
   test("NOT BETWEEN date_trunc_ym") {
-    val q = "SELECT ward, count(*) AS count WHERE crime_date NOT BETWEEN date_trunc_ym('2011-02-01') " +
-      "AND date_trunc_ym('2012-05-02') GROUP BY ward"
-    val queryAnalysis = analyzeQuery(q)
+
+    val queryAnalysis = analyzeQuery(qNotBetweenDateTruncYm)
 
     val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
 
