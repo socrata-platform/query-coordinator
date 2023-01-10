7d58f7b4563a7fc6ce323011d3dc0ad3d204a3db
dmitry-at-tyler <55448085+dmitry-at-tyler@users.noreply.github.com>
2023-01-10 16:48:11 -0500

    Adding configurable tests for rollup query rewriting (#276)
    Adding configurable query rewriter test, to decouple test implementation from test configuration. System under test (i.e. Rewriter) supposed to do 1 thing - rewrite a query given the database schema and rollup queries. So, test configuration have just that. Additional queries/rollups/schemas test cases could be added just by adding / changing config files.
    
    Reviewed-by: Zach @zachlefevre

---
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/rollups/TestRollupQueryRewriter.scala
@@ -0,0 +1,182 @@
+package com.socrata.querycoordinator.rollups
+
+import com.rojoma.json.v3.util.{AutomaticJsonCodecBuilder, JsonUtil}
+import com.socrata.querycoordinator.QueryRewriter.{ColumnId, RollupName}
+import com.socrata.querycoordinator._
+import com.socrata.soql._
+import com.socrata.soql.collection.OrderedMap
+import com.socrata.soql.environment.{ColumnName, DatasetContext}
+import com.socrata.soql.functions.{SoQLFunctionInfo, SoQLTypeInfo, SoQLFunctions}
+import com.socrata.soql.parsing.{AbstractParser, Parser}
+import com.socrata.soql.typed.CoreExpr
+import com.socrata.soql.types.{SoQLType, SoQLValue}
+
+import scala.io.Source
+
+
+case class TestCase(query: String, rewrites: Map[String, String])
+object TestCase {
+  implicit val jCodec = AutomaticJsonCodecBuilder[TestCase]
+}
+
+case class TestConfig(schemas: Map[String, Map[String, (SoQLType, String)]], rollups: Map[String, String], tests: Seq[TestCase])
+object TestConfig {
+  implicit val jCodec = AutomaticJsonCodecBuilder[TestConfig]
+}
+
+
+class TestRollupQueryRewriter extends TestBase {
+
+  //
+  // System Under Test
+  //
+  val analyzer = new SoQLAnalyzer(SoQLTypeInfo, SoQLFunctionInfo)
+  val rewriter = new QueryRewriterWithJoinEnabled(analyzer)
+
+  //
+  //  Helper methods
+  //
+  private def analyzeQuery(q: String,
+                   context: AnalysisContext[SoQLType, SoQLValue],
+                   columnMapping: Map[QualifiedColumnName, String]):
+  BinaryTree[SoQLAnalysis[ColumnId, SoQLType]] = {
+
+    val parserParams = AbstractParser.Parameters(allowJoins = true)
+    val parsed = new Parser(parserParams).binaryTreeSelect(q)
+    val analyses = analyzer.analyzeBinary(parsed)(context)
+    val merged = SoQLAnalysis.merge(SoQLFunctions.And.monomorphic.get, analyses)
+    QueryParser.remapAnalyses(columnMapping, merged)
+  }
+
+  private def analyzeRewrittenQuery(q: String,
+                            ruQuery: String,
+                            context: AnalysisContext[SoQLType, SoQLValue],
+                            columnMapping: Map[QualifiedColumnName, String]):
+  BinaryTree[SoQLAnalysis[ColumnId, SoQLType]] = {
+
+    val parserParams =
+      AbstractParser.Parameters(
+        allowJoins = true
+      )
+    val parsed = new Parser(parserParams).binaryTreeSelect(q)
+    val ruCtx = rollupContext(ruQuery, context, columnMapping)
+    val ctx = context.withUpdatedSchemas(_ ++ ruCtx)
+    val result = analyzer.analyzeBinary(parsed)(ctx)
+    val merged = SoQLAnalysis.merge(SoQLFunctions.And.monomorphic.get, result)
+    val columnMap = columnMapping ++ rollupColumnIds(ruQuery, context, columnMapping)
+    QueryParser.remapAnalyses(columnMap, merged)
+  }
+
+  private def rollupColumnIds(ruQuery: String,
+                      context: AnalysisContext[SoQLType, SoQLValue],
+                      columnMapping: Map[QualifiedColumnName, String]): Map[QualifiedColumnName, String] = {
+    rollupSchema(ruQuery, context, columnMapping).map {
+      case (cn, _typ) =>
+        QualifiedColumnName(None, cn) -> cn.name
+    }
+  }
+
+  private def rollupContext(ruQuery: String,
+                    context: AnalysisContext[SoQLType, SoQLValue],
+                    columnMapping: Map[QualifiedColumnName, String]): Map[String, DatasetContext[SoQLType]] = {
+    val map = rollupSchema(ruQuery, context, columnMapping)
+    Map("_" -> new DatasetContext[SoQLType] {
+      val schema = OrderedMap[ColumnName, SoQLType](map.toSeq: _*)
+    })
+  }
+
+  private def rollupSchema(ruQuery: String,
+                   context: AnalysisContext[SoQLType, SoQLValue],
+                   columnMapping: Map[QualifiedColumnName, String]): Map[ColumnName, SoQLType] = {
+    val ruAnalyses = analyzeQuery(ruQuery, context, columnMapping).outputSchema.leaf
+    ruAnalyses.selection.values.zipWithIndex.map {
+      case (expr: CoreExpr[_, _], idx) =>
+        (ColumnName(s"c${idx + 1}"), expr.typ.t)
+    }.toMap
+  }
+
+  //
+  //  Main test method
+  //
+  private def loadAndRunTests(configFile: String) {
+    val resource = Source.fromResource(configFile)
+    val config = JsonUtil.readJson[TestConfig](resource.reader()).right.get
+
+    val allDatasetContext = AnalysisContext[SoQLType, SoQLValue](
+      schemas = config.schemas.mapValues { schemaWithFieldName =>
+        val columnNameToType = schemaWithFieldName.map {
+          case (_iColumnId, (typ, fieldName)) =>
+            (ColumnName(fieldName), typ)
+        }
+        new DatasetContext[SoQLType] {
+          val schema: OrderedMap[ColumnName, SoQLType] =
+            OrderedMap[ColumnName, SoQLType](columnNameToType.toSeq: _*)
+        }
+      },
+      parameters = ParameterSpec.empty
+    )
+
+    val allQualifiedColumnNameToIColumnId: Map[QualifiedColumnName, String] = config.schemas.foldLeft(Map.empty[QualifiedColumnName, String]) { (acc, entry) =>
+      val (tableName, schemaWithFieldName) = entry
+      val map = schemaWithFieldName.map {
+        case (iColumnId, (_typ, fieldName)) =>
+          val qual = if (tableName == "_") None else Some(tableName)
+          QualifiedColumnName(qual, ColumnName(fieldName)) -> iColumnId
+      }
+      acc ++ map
+    }
+
+    val rollupAnalysis = QueryRewriter.mergeRollupsAnalysis(
+      config.rollups.map {
+        case (ruName, ruSoql) =>
+          val x = new RollupName(ruName)
+          (x, analyzeQuery(ruSoql, allDatasetContext, allQualifiedColumnNameToIColumnId))
+      }
+    )
+
+    config.tests.foreach(test => {
+      val queryAnalysis = analyzeQuery(test.query, allDatasetContext, allQualifiedColumnNameToIColumnId).outputSchema.leaf
+      val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+
+      rewrites should have size test.rewrites.size
+
+      test.rewrites.foreach { case (rollupName, rewrittenQuery) =>
+        val rewrittenQueryAnalysis = analyzeRewrittenQuery(
+          rewrittenQuery, config.rollups(rollupName), allDatasetContext, allQualifiedColumnNameToIColumnId
+        ).outputSchema.leaf
+
+        rewrites should contain key rollupName
+        rewrites.get(rollupName).get should equal(rewrittenQueryAnalysis)
+      }
+
+    })
+  }
+
+  //
+  //  Tests
+  //
+
+  test("rollup rewriter tests") {
+    loadAndRunTests("rollups/query_rewriter_test_configs/test_set_1.json")
+  }
+
+  test("rollup rewriter dates tests") {
+    loadAndRunTests("rollups/query_rewriter_test_configs/test_dates_1.json")
+  }
+
+  test("piped rollup queries") {
+    loadAndRunTests("rollups/query_rewriter_test_configs/test_pipes_1.json")
+  }
+
+  test("piped rollup queries 2") {
+    loadAndRunTests("rollups/query_rewriter_test_configs/test_pipes_2.json")
+  }
+
+  //
+  // ToDo: rollup rewriter works, but rewrited query analyzer don't
+  //
+  //  test("rollup rewriter tests 2") {
+  //    loadAndRunTests("rollups/query_rewriter_test_configs/test_set_2.json")
+  //  }
+
+}
