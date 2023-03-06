191d57a449746905b8a345913b33398cb7240146
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-03-06 17:03:50 -0500

    use get -> option method instead of try

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/rollups/ClauseAwareExpressionRewriter.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/rollups/ClauseAwareExpressionRewriter.scala
@@ -5,8 +5,6 @@ import com.socrata.querycoordinator.rollups.QueryRewriterImplementation.{Functio
 import com.socrata.querycoordinator.util.Join.NoQualifier
 import com.socrata.soql.typed
 
-import scala.util.{Failure, Success, Try}
-
 /*
 This class handles a specific case of the ExpressionRewriter where we know that all clauses (where/groupby/having) match,
 so therefor we want to rewrite the window functions.
@@ -16,20 +14,16 @@ The decision of which class to use is made in com.socrata.querycoordinator.rollu
 class ClauseAwareExpressionRewriter(override val rollupColumnId: (Int) => String,
                                     override val rewriteWhere: (Option[Expr], Analysis, Map[Expr, Int]) => Option[Where]) extends ExpressionRewriter(rollupColumnId, rewriteWhere) {
   /** Recursively maps the Expr based on the rollupColIdx map, returning either
-   * a mapped expression or None if the expression couldn't be mapped.
-   *
-   * Note that every case here needs to ensure to map every expression recursively
-   * to ensure it is either a literal or mapped to the rollup.
-   */
-   override def apply(e: Expr, r: Analysis, rollupColIdx: Map[Expr, Int]): Option[Expr] = {
-    e match{
+    * a mapped expression or None if the expression couldn't be mapped.
+    *
+    * Note that every case here needs to ensure to map every expression recursively
+    * to ensure it is either a literal or mapped to the rollup.
+    */
+  override def apply(e: Expr, r: Analysis, rollupColIdx: Map[Expr, Int]): Option[Expr] = {
+    e match {
       case fc: FunctionCall if fc.window.nonEmpty =>
-        Try(rollupColIdx(fc))match{
-          case Success(remappedColumn)=>Some(typed.ColumnRef(NoQualifier, rollupColumnId(remappedColumn), fc.typ)(fc.position))
-          case Failure(e:NoSuchElementException)=>None
-          case Failure(e)=> throw e
-        }
-      case _=>
+        rollupColIdx.get(fc).map(cid => typed.ColumnRef(NoQualifier, rollupColumnId(cid), fc.typ)(fc.position))
+      case _ =>
         super.apply(e, r, rollupColIdx)
     }
   }
