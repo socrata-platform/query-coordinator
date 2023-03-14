b30ec50ffed559d9bbe6b51413f34a7707c298bd
PaulMTT <109166392+PaulMTT@users.noreply.github.com>
2023-03-14 11:24:50 -0400

    EN-55220: Column name mapping fix, EN-56820 window function fix (#297)

--- query-coordinator/src/test/scala/com/socrata/querycoordinator/QueryRewriterWindowFunctionTest.scala
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/QueryRewriterWindowFunctionTest.scala
@@ -102,6 +102,44 @@ class QueryRewriterWindowFunctionTest extends FunSuite {
     )
   }
 
+  test("Window function should rewrite when groupby clauses match, even with an extra column in the rollup") {
+
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "depname_column" -> (SoQLText.t, "depname"),
+          "empno_column" -> (SoQLNumber.t, "empno"),
+          "salary_column" -> (SoQLNumber.t, "salary")
+        )
+      ),
+      Map(
+        "one" -> "SELECT depname, empno, sum(salary) as salarySum, 1 as extraColumn, avg(salarySum) OVER (PARTITION BY depname) as avgSalary group by depname,empno"
+      ),
+      "SELECT depname, empno, sum(salary) as salarySum, avg(salarySum) OVER (PARTITION BY depname) as avgSalary group by depname,empno",
+      "select c1 as depname, c2 as empno, c3 as salarySum, c5 as avgSalary",
+      Some("one")
+    )
+  }
+
+  test("Window function should rewrite when groupby clauses match, even with an extra column in the query") {
+
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "depname_column" -> (SoQLText.t, "depname"),
+          "empno_column" -> (SoQLNumber.t, "empno"),
+          "salary_column" -> (SoQLNumber.t, "salary")
+        )
+      ),
+      Map(
+        "one" -> "SELECT depname, empno, sum(salary) as salarySum, avg(salarySum) OVER (PARTITION BY depname) as avgSalary group by depname,empno"
+      ),
+      "SELECT depname, empno, sum(salary) as salarySum, 1 as extraColumn, avg(salarySum) OVER (PARTITION BY depname) as avgSalary group by depname,empno",
+      "select c1 as depname, c2 as empno, c3 as salarySum, 1 as extraColumn, c4 as avgSalary",
+      Some("one")
+    )
+  }
+
   test("Mismatch of groupby with window function should not rewrite") {
 
     AssertRewriteDefault(
@@ -176,6 +214,63 @@ class QueryRewriterWindowFunctionTest extends FunSuite {
       ),
       "SELECT depname, empno, salary, (1 + avg(salary) OVER (PARTITION BY depname)) as avgSalary",
       "select c1 as depname, c2 as empno, c3 as salary, c4 as avgSalary",
+      Some("one")
+    )
+  }
+
+  test("Window function should rewrite when there are no clauses and its far from the top level") {
+
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "depname_column" -> (SoQLText.t, "depname"),
+          "empno_column" -> (SoQLNumber.t, "empno"),
+          "salary_column" -> (SoQLNumber.t, "salary")
+        )
+      ),
+      Map(
+        "one" -> "SELECT depname, empno, salary, (1 + (1 + (1 + (1 + avg(salary) OVER (PARTITION BY depname))))) as avgSalary"
+      ),
+      "SELECT depname, empno, salary, (1 + (1 + (1 + (1 + avg(salary) OVER (PARTITION BY depname))))) as avgSalary",
+      "select c1 as depname, c2 as empno, c3 as salary, c4 as avgSalary",
+      Some("one")
+    )
+  }
+
+  test("Window function should rewrite when there are no clauses and its not top level, even with an extra column in the rollup") {
+
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "depname_column" -> (SoQLText.t, "depname"),
+          "empno_column" -> (SoQLNumber.t, "empno"),
+          "salary_column" -> (SoQLNumber.t, "salary")
+        )
+      ),
+      Map(
+        "one" -> "SELECT depname, empno, salary,1 as extraColumn, (1 + avg(salary) OVER (PARTITION BY depname)) as avgSalary"
+      ),
+      "SELECT depname, empno, salary, (1 + avg(salary) OVER (PARTITION BY depname)) as avgSalary",
+      "select c1 as depname, c2 as empno, c3 as salary, c5 as avgSalary",
+      Some("one")
+    )
+  }
+
+  test("Window function should rewrite when there are no clauses and its not top level, even with an extra column in the query") {
+
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "depname_column" -> (SoQLText.t, "depname"),
+          "empno_column" -> (SoQLNumber.t, "empno"),
+          "salary_column" -> (SoQLNumber.t, "salary")
+        )
+      ),
+      Map(
+        "one" -> "SELECT depname, empno, salary, (1 + avg(salary) OVER (PARTITION BY depname)) as avgSalary"
+      ),
+      "SELECT depname, empno, salary, 1 as extraColumn, (1 + avg(salary) OVER (PARTITION BY depname)) as avgSalary",
+      "select c1 as depname, c2 as empno, c3 as salary, 1 as extraColumn, c4 as avgSalary",
       Some("one")
     )
   }
