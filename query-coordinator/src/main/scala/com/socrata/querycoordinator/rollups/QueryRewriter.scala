1b03b6ee4a58da4bf7e77c4269848892f16765f5
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-03-16 19:21:46 -0400

    remove the junk

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/rollups/QueryRewriter.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/rollups/QueryRewriter.scala
@@ -1,12 +1,10 @@
 package com.socrata.querycoordinator.rollups
 
 import com.socrata.querycoordinator.rollups.QueryRewriter.{Analysis, AnalysisTree, Expr, RollupName}
-import com.socrata.querycoordinator.util.BinaryTreeHelper
 import com.socrata.querycoordinator.{Schema, SchemaWithFieldName}
-import com.socrata.soql.collection.OrderedMap
 import com.socrata.soql.environment.{ColumnName, TableName}
 import com.socrata.soql.functions.SoQLFunctions
-import com.socrata.soql.{BinaryTree, Leaf, PipeQuery, SoQLAnalysis, typed}
+import com.socrata.soql.{BinaryTree, SoQLAnalysis}
 import com.socrata.soql.types.SoQLType
 
 /**
@@ -62,52 +60,14 @@ object QueryRewriter {
     * Merge rollups analysis
     */
   def mergeRollupsAnalysis(rus: Map[RollupName, AnalysisTree]): Map[RollupName, Analysis] = {
-    rus.mapValues { bt =>
-      val merged = doMerge(
-        bt.map(a => a.mapColumnIds((columnId, _) => ColumnName(columnId)))
-      )
-
-      val mergedAgain = SoQLAnalysis.merge(
+    rus.mapValues(bt =>
+      SoQLAnalysis.merge(
         SoQLFunctions.And.monomorphic.get,
-        merged
-      )
-//
-//      val mergedMap = merged match {
-//        case PipeQuery(l, r) =>
-//          l.outputSchema.leaf.selection.foreach { case (b,c) =>
-//            BinaryTreeHelper.replace(r, r.outputSchema.leaf, r.outputSchema.leaf.copy(selection = {
-//              r.outputSchema.leaf.selection.map(doRemap(l.outputSchema.leaf.selection))
-//            }))
-//          }
-//        case x => x
-//      }
-//            val mergedMap = merged match {
-//              case PipeQuery(l, r) => BinaryTreeHelper.replace(r, r.outputSchema.leaf, r.outputSchema.leaf.copy(selection = {
-//                r.outputSchema.leaf.selection.map(doRemap(l.outputSchema.leaf.selection))
-//              }))
-//              case x => x
-//            }
-
-      merged.outputSchema.leaf.mapColumnIds((columnName, _) => columnName.name)
-    }
+        bt.map(a => a.mapColumnIds((columnId, _) => ColumnName(columnId))
+        )
+      ).outputSchema.leaf.mapColumnIds((columnName, _) => columnName.name)
+    )
   }
-
-  def doRemap(schema:OrderedMap[ColumnName, typed.CoreExpr[ColumnName, SoQLType]])(selection:(ColumnName, typed.CoreExpr[ColumnName, SoQLType])):(ColumnName, typed.CoreExpr[ColumnName, SoQLType])={
-    val (columnName,expression) = selection
-    ???
-  }
-
-  def doMerge(analysisTree: BinaryTree[SoQLAnalysis[ColumnName, SoQLType]]): BinaryTree[SoQLAnalysis[ColumnName, SoQLType]] = {
-    SoQLAnalysis.merge(
-      SoQLFunctions.And.monomorphic.get,
-      analysisTree
-    ) match {
-      case PipeQuery(l @ PipeQuery(_, _), r) =>
-        doMerge(PipeQuery(doMerge(l), r))
-      case other => other
-    }
-  }
-
 
   def primaryRollup(names: Seq[String]): Option[String] = {
     names.filterNot(_.startsWith(TableName.SoqlPrefix)).headOption
