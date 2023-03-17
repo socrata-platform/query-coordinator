004ee9c73a1f2f2940315446eb0a588da324e3c9
Dmitry Guglya <dmitry.guglya@tylertech.com>
2023-03-17 02:11:49 -0400

    adding doc for test

--- query-coordinator/src/test/scala/com/socrata/querycoordinator/rollups/QueryRewriterTest.scala
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/rollups/QueryRewriterTest.scala
@@ -9,6 +9,37 @@ import com.socrata.soql.functions.{SoQLFunctionInfo, SoQLFunctions, SoQLTypeInfo
 import com.socrata.soql.parsing.{AbstractParser, Parser}
 import com.socrata.soql.types.{SoQLType, SoQLValue}
 
+/**
+  *  How to use QueryRewriter test:
+  *
+  *  If rollup does not match query, follow the steps below to investigate:
+  *
+  *  1. Create new test case in resources/rollups/query_rewriter_real_test_configs
+  *     Test case json file format:
+  *     {
+  *       "schemas": {
+  *         "_": {                                   <- root schema
+  *           "2dup-k82r": ["number", "rowkey"]      <- column fxf, column type and column name
+  *         },
+  *         "ep9t-zk9z": {
+  *           "47rk-qiqd": ["text", "county"]
+  *         }
+  *       },
+  *       "rollups": {                                <- rollup as stored in pg_secondary rollup_map
+  *         "r1": "SELECT `_93hn-8t73` AS `casenumber`, max(`_u4tu-5655`) AS `submitted_` GROUP BY `casenumber`"
+  *       },
+  *       "tests": [                                  <- multiple tests - query and rewrites
+  *         {
+  *           "query": "SELECT `casenumber` AS `casenumber`, max(`submitted`) AS `submitted_` GROUP BY `casenumber`",
+  *           "rewrites": {
+  *             "r1": "SELECT ListMap(casenumber -> c1 :: text, submitted_ -> c2 :: floating_timestamp)"
+  *         }
+  *       ]
+  *     }
+  *  2. Get the column names, fxf and types from the pg_secondary rollup_map and column_map
+  *       (filter column map by dataset id)
+  *  3. Keep test case files once done debugging.
+  */
 
 class QueryRewriterTest extends BaseConfigurableRollupTest {
 
