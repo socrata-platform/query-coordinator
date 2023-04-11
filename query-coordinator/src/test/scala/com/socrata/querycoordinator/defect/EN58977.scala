a041ece26bfe5c21aa7ce2d6c19e56e8848e8479
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-04-11 09:50:34 -0400

    added test for fixed rollup/query

--- query-coordinator/src/test/scala/com/socrata/querycoordinator/defect/EN58977.scala
+++ query-coordinator/src/test/scala/com/socrata/querycoordinator/defect/EN58977.scala
@@ -63,4 +63,52 @@ class EN58977 extends FunSuite{
     )
   }
 
+  test("example two, with 'casecategorycode' added to the rollup. Should rewrite.") {
+    AssertRewriteDefault(
+      Map(
+        "_" -> Map(
+          "rowkey_column" -> (SoQLNumber.t, "rowkey"),
+          "efspname_column" -> (SoQLText.t, "efspname"),
+          "county_column" -> (SoQLText.t, "county"),
+          "office_column" -> (SoQLText.t, "office"),
+          "orgchartname_column" -> (SoQLText.t, "orgchartname"),
+          "nodeid_column" -> (SoQLNumber.t, "nodeid"),
+          "casenumber_column" -> (SoQLText.t, "casenumber"),
+          "envelopeid_column" -> (SoQLNumber.t, "envelopeid"),
+          "isinitial_column" -> (SoQLBoolean.t, "isinitial"),
+          "filingid_column" -> (SoQLNumber.t, "filingid"),
+          "filingtype_column" -> (SoQLText.t, "filingtype"),
+          "submitted_column" -> (SoQLFloatingTimestamp.t, "submitted"),
+          "submittedday_column" -> (SoQLText.t, "submittedday"),
+          "filingstate_column" -> (SoQLText.t, "filingstate"),
+          "casecategorycode_column" -> (SoQLText.t, "casecategorycode"),
+          "casetypecode_column" -> (SoQLText.t, "casetypecode"),
+          "filingcode_column" -> (SoQLText.t, "filingcode"),
+          "isindividualfirm_column" -> (SoQLBoolean.t, "isindividualfirm"),
+          "username_column" -> (SoQLText.t, "username"),
+          "firmid_column" -> (SoQLNumber.t, "firmid"),
+          "firmname_column" -> (SoQLText.t, "firmname"),
+          "rejectionreason_column" -> (SoQLText.t, "rejectionreason"),
+          "numberofdocuments_column" -> (SoQLNumber.t, "numberofdocuments"),
+          "underreview_column" -> (SoQLFloatingTimestamp.t, "underreview"),
+          "reviewed_column" -> (SoQLFloatingTimestamp.t, "reviewed"),
+          "primaryamount_column" -> (SoQLNumber.t, "primaryamount"),
+          "secondaryamount_column" -> (SoQLNumber.t, "secondaryamount"),
+          "systempaymentaccounttype_column" -> (SoQLText.t, "systempaymentaccounttype"),
+          "adjustedsubmitdate_column" -> (SoQLFloatingTimestamp.t, "adjustedsubmitdate"),
+          "clerkresponsetime_column" -> (SoQLNumber.t, "clerkresponsetime"),
+          "insertedby_column" -> (SoQLText.t, "insertedby"),
+          "clerkname_column" -> (SoQLText.t, "clerkname"),
+
+        )
+      ),
+      Map(
+        "one" -> "SELECT `casecategorycode`,`clerkname` AS `clerkname`, date_trunc_ymd(`submitted`) AS `date_trunc_ymd_submitted`, date_trunc_ym(`submitted`) AS `date_trunc_ym_submitted`, date_trunc_y(`submitted`) AS `date_trunc_y_submitted`, date_extract_woy(`submitted`) AS `date_extract_woy_submitted`, min(`submitted`) AS `min_submitted`, max(`submitted`) AS `max_submitted`, count(*) AS `count`, count(`filingid`) AS `count_filingid`, count(`filingstate`) AS `count_filingstate`, count(`clerkname`) AS `count_clerkname` GROUP BY `casecategorycode`, `clerkname`, `date_trunc_ymd_submitted`, `date_trunc_ym_submitted`, `date_trunc_y_submitted`, `date_extract_woy_submitted`"
+      ),
+      "select count(*) group by casecategorycode |> select count(*) as rows",
+      "select coalesce(sum(c9),0) as count group by c1 |> select count(*) as rows",
+      Some("one")
+    )
+  }
+
 }
