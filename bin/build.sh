6d37c85340f39c82be826bd2acd46fa9f3e4af98
rjmac <robert.macomber@socrata.com>
2023-08-22 10:42:28 -0700

    Update bin scripts to reflect the new simplified assembly naming (#344)

--- bin/build.sh
+++ bin/build.sh
@@ -5,14 +5,13 @@ REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BASEDIR="$(dirname "${REALPATH}")/.."
 
 cd "$BASEDIR"
-JARFILE="$(ls -rt query-coordinator/target/scala-*/query-coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
+JARFILE="query-coordinator/target/query-coordinator-assembly.jar"
 SRC_PATHS=($(find .  -maxdepth 2 -name 'src' -o -name '*.sbt' -o -name '*.scala'))
-if [ -z "$JARFILE" ] || find "${SRC_PATHS[@]}" -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
+if [ ! -f "$JARFILE" ] || find "${SRC_PATHS[@]}" -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
     if [ "$1" == '--fast' ]; then
         echo 'Assembly is out of date.  Fast start is enabled so skipping rebuild anyway...' >&2
     else
         nice -n 19 sbt assembly >&2
-        JARFILE="$(ls -rt query-coordinator/target/scala-*/query-coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
         touch "$JARFILE"
     fi
 fi
