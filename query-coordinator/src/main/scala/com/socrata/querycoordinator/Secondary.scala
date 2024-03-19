6dd566dfa8c2eecb383c0fb4f81c63b042182fde
zlef <zachlefevre@gmail.com>
2024-03-19 12:22:03 -0500

    print providers

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/Secondary.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/Secondary.scala
@@ -40,7 +40,7 @@ class Secondary(secondaryProvider: ServiceProviderProvider[AuxiliaryData],
       instance <- Option(secondaryProvider.provider(name).getInstance())
     } yield instance
 
-    log.info(s"${dataset}: Found instance ${instance} for ${instanceName}. markBroken is ${markBrokenOnUnknown}")
+    log.info(s"${dataset}: Found instance ${instance} for ${instanceName}. markBroken is ${markBrokenOnUnknown}. Allowed instances are ${secondaryProvider.providers}")
 
     if (markBrokenOnUnknown && instance.isEmpty) {
       instanceName.foreach { n => secondaryInstance.flagError(dataset, n) }
