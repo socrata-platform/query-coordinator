f2311f854841b6efdde0b16092ace4c026cdbe29
zlef <me@zacharylefevre.com>
2025-03-14 15:45:43 -0500

    sad message

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/resources/DeathResource.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/resources/DeathResource.scala
@@ -14,6 +14,7 @@ class DeathResource extends QCResource {
 
   override val get: HttpService = {
     _: HttpRequest => {
+      log.info(s"you've asked me to die")
       val state = com.socrata.querycoordinator.State.state
       log.info(s"old state is $state")
       state.beDead = !state.beDead
