38d919df7a66f75c2c0352ccde801a329f60ae97
dmitry-at-tyler <55448085+dmitry-at-tyler@users.noreply.github.com>
2026-03-10 21:50:46 -0400

    [EN-88804] adding 'X-Socrata-Resource' into logging (#429)

--- query-coordinator/src/main/scala/com/socrata/querycoordinator/resources/NewQueryResource.scala
+++ query-coordinator/src/main/scala/com/socrata/querycoordinator/resources/NewQueryResource.scala
@@ -319,7 +319,10 @@ class NewQueryResource(
 
         def chooseSecondaries(): Either[HttpResponse, Seq[FoundSecondary[String]]] = {
           val tables = analysis.statement.allTables.map(_.name)
-          org.slf4j.MDC.put("X-Socrata-Resource", tables.mkString(" "))
+
+          // !!! THE LINE BELOW IS IMPORTANT - DO NOT REMOVE !!!
+          org.slf4j.MDC.put("X-Socrata-Resource", tables.map(_._1.underlying).mkString(" "))
+
           store match {
             case None =>
               if(tables.isEmpty) {
